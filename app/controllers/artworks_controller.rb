2fc6a5769035d751e8cc66b40d1f029d07296e52
sebastosh <sebastosh@gmail.com>
2019-07-10 17:46:58 -0400

    artworks create

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,22 @@
 class ArtworksController < ApplicationController
+
+    def index
+        artworks = Artwork.all
+        render json: ArtworkSerializer.new(artworks)
+      end
+
+    def show
+        artwork = Artwork.find(params[:id])
+        render json: ArtworkSerializer.new(artwork)
+      end
+    
+    def create
+        artwork = Artwork.create(artwork_params)
+        render json: artwork
+    end
+
+    def artwork_params
+        params.require(:artwork).permit!
+      end
+
 end
