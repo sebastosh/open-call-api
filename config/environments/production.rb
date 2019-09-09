593a78cb8dd740cd0727dcbe5395024a9d28aef8
sebastosh <sebastosh@gmail.com>
2019-09-09 08:15:59 -0400

    heroku finally fixed2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
 config.default_url_options = { host: "https://frozen-lake-42735.herokuapp.com/" }
-config.action_mailer.default_url_options = { :host => "https://frozen-lake-42735.herokuapp.com/" 
+config.action_mailer.default_url_options = { :host => "https://frozen-lake-42735.herokuapp.com/" }
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
