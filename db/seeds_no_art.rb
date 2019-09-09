3e174a2819bf66cba5971fa225ffe6e2829fd336
sebastosh <sebastosh@gmail.com>
2019-09-08 22:38:50 -0400

    heroku prep2

--- db/seeds_no_art.rb
+++
@@ -1,9 +0,0 @@
-Org.create(name: "org name 00", mission: "org mission 00", street: "Org Street 00", city: "org city 00", zip: 11230, country: "org country 00" )
-Org.create(name: "org name 01", mission: "org mission 01", street: "Org Street 01", city: "org city 01", zip: 11231, country: "org country 01" )
-Org.create(name: "org name 02", mission: "org mission 02", street: "Org Street 02", city: "org city 02", zip: 11232, country: "org country 02" )
-Org.create(name: "org name 03", mission: "org mission 03", street: "Org Street 03", city: "org city 03", zip: 11233, country: "org country 03" )
-
-    Call.create(name: "call name 00", description: "call description 00", medium: "call medium 0", deadline:  Date.new(2019,9,5), org_id: 1 )
-    Call.create(name: "call name 01", description: "call description 01", medium: "call medium 1", deadline:  Date.new(2019,9,1), org_id: 2 )
-    Call.create(name: "call name 02", description: "call description 02", medium: "call medium 2", deadline:  Date.new(2019,9,2), org_id: 3 )
-    Call.create(name: "call name 03", description: "call description 03", medium: "call medium 3", deadline:  Date.new(2019,9,3), org_id: 4 )
